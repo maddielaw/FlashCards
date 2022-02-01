b7807d0111b0be547a1413cb680a40c57a0fad4e
Madison Law <madisonlaw1@gmail.com>
2022-01-31 20:49:20 -0700

    Rename cards array in Deck test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,5 +2,45 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 
+describe('Deck', function() {
+
+  it.skip('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it.skip('should have cards inside it', function() {
+    const cards = [{
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
+    }, {
+      "id": 2,
+      "question": "What is a comma-separated list of related values?",
+      "answers": ["array", "object", "function"],
+      "correctAnswer": "array"
+    }];
+
+    const deck = new Deck(cards);
+    expect(deck.cards).to.equal(cards);
+  });
+
+  it.skip('should be able to count cards in deck', function() {
+    const cards = [{
+      "id": 1,
+      "question": "What allows you to define a set of related information using key-value pairs?",
+      "answers": ["object", "array", "function"],
+      "correctAnswer": "object"
+    }, {
+      "id": 2,
+      "question": "What is a comma-separated list of related values?",
+      "answers": ["array", "object", "function"],
+      "correctAnswer": "array"
+    }];
+
+    const deck = new Deck(cards);
+    expect(deck.countCards()).to.equal(2)
+  });
+});
