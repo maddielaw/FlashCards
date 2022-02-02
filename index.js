dd6c552b0d6605c53a3f2666b0f6697c2a7cd630
Madison Law <madisonlaw1@gmail.com>
2022-02-02 14:52:03 -0700

    Add code to start game in command line

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// This is where your project starts.
+// console.log('Your project is running...'); 
 
-console.log('Your project is running...'); 
+const Card = require('./src/Card');
+const data = require('./src/data');
+const Deck = require('./src/Deck');
+const Round = require('./src/Round');
+const Game = require('./src/Game')
+// const prototypeQuestions = data.prototypeData;
+const util = require('./src/util');
+
+
+const currentGame = new Game();
+
+currentGame.start()
+
+
