13454fd428fdf31411ecceb42882b5f3901dc316
Madison Law <madisonlaw1@gmail.com>
2022-02-01 17:57:09 -0700

    Add Turn implementation code

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,35 +6,35 @@ const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
-    expect(turn).to.be.a('function');
+    expect(Turn).to.be.a('function');
   });
 
-  it.skip('should contain a user guess', function() {
+  it('should contain a user guess', function() {
     const turn = new Turn('array');
     expect(turn.guess).to.equal('array');
   });
 
-  it.skip('should instantiate a new Card object', function() {
+  it('should instantiate a new Card object', function() {
     const card = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const turn = new Turn('array', card);
     expect(turn.card).to.be.an.instanceOf(Card);
   });
 
-  it.skip('should be able to return the guess', function() {
+  it('should be able to return the guess', function() {
     const card = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const turn = new Turn('array', card);
     expect(turn.returnGuess()).to.equal('array');
   });
 
-  it.skip('should be able to return the current card', function() {
+  it('should be able to return the current card', function() {
     const card = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const turn = new Turn('array', card);
     expect(turn.returnCard()).to.equal(card)
   });
 
-  it.skip('should be able to evaluate whether the guess is correct or not', function() {
+  it('should be able to evaluate whether the guess is correct or not', function() {
     const card = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const turn1 = new Turn('array', card);
     const turn2 = new Turn('object', card);
@@ -43,7 +43,7 @@ describe('Turn', function() {
     expect(turn2.evaluateGuess()).to.equal(false);
   });
 
-  it.skip('should be able to alert user of correct or incorrect answer', function() {
+  it('should be able to alert user of correct or incorrect answer', function() {
     const card = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const turn1 = new Turn('array', card);
     const turn2 = new Turn('object', card);
