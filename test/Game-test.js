fea7bce748a7beabee8187cd5933c1f807050b9c
Madison Law <madisonlaw1@gmail.com>
2022-02-02 14:38:45 -0700

    Add implementation code for Game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -5,6 +5,7 @@ const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
+const Game = require('../src/Game');
 
 describe('Game', function() {
   // this.beforeEach(function() {
@@ -17,7 +18,7 @@ describe('Game', function() {
   //   const round = new Round(deck);
   // })
 
-  it.skip('should be able to create a deck of cards', function() {
+  it('should be able to create a deck of cards', function() {
     const card1 = new Card(1,"What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
     const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
     const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
@@ -25,13 +26,24 @@ describe('Game', function() {
     const cards = [card1, card2, card3];
     const deck = new Deck(cards);
     const round = new Round(deck);
+    const game = new Game();
 
     game.start();
     expect(deck.cards[0]).to.be.an.instanceOf(Card)
+    expect(deck.cards.length).to.equal(3);
   });
 
-  it.skip('should be able to start a new round', function() {
-   
+  it('should be able to start a new round', function() {
+    const card1 = new Card(1,"What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
+    const card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+  
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    
+    const game = new Game();
+
     expect(game.currentRound).to.equal(null)
     game.start();
     expect(game.currentRound).to.be.instanceOf(Round)
