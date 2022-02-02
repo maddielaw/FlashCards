3798e8d7313039a67d558ce43715b9200ca6600d
Madison Law <madisonlaw1@gmail.com>
2022-02-02 14:12:40 -0700

    Add code to move currentCard to next card in deck

--- src/Round.js
+++ src/Round.js
@@ -7,28 +7,34 @@ class Round {
     this.currentCard = deck.cards[this.cardCounter];
     this.turns = 0;
     this.incorrectGuesses = [];
-  }
+  };
+
   returnCurrentCard() {
     return this.currentCard;
-  }
+  };
+
   takeTurn(guess) {
     let currentTurn = new Turn(guess, this.currentCard);
     this.turns ++;
-    this.cardCounter ++;
-    if (currentTurn.guess === this.currentCard.correctAnswer) {
+
+    if (currentTurn.evaluateGuess()) {
+      this.currentCard = this.deck.cards[++ this.cardCounter]
       return "correct!"
     } else {
+      this.currentCard = this.deck.cards[++ this.cardCounter]
       this.incorrectGuesses.push(this.currentCard.id);
       return "incorrect!"
     }
-  }
+  };
+
   calculatePercentageCorrect() {
     return (this.incorrectGuesses.length / this.turns) * 100
-  }
+  };
+
   endRound() {
     return `** Round over! ** You answered ${this.calculatePercentageCorrect()}% of the questions correctly!`
   }
-}
+};
 
 
 module.exports = Round;
