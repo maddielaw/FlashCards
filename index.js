5872e747b2ed587cca997c7b4f06babe3258118b
Madison Law <madisonlaw1@gmail.com>
2022-02-03 13:29:46 -0700

    Remove commented out code from index file

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// console.log('Your project is running...'); 
-
-const Card = require('./src/Card');
-const data = require('./src/data');
-const Deck = require('./src/Deck');
-const Round = require('./src/Round');
 const Game = require('./src/Game')
-// const prototypeQuestions = data.prototypeData;
-const util = require('./src/util');
-
 
 const currentGame = new Game();
 
-currentGame.start()
+currentGame.start();
 
 
